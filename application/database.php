cb34f3e51f273b53f4335ebb04e61489b66290c0
zeng342541425 <342541425@qq.com>
2018-01-22 15:18:32 +0800

    添加数据库忽略文件

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '11111111111',
+    'hostname'        => '2222222222',
     // 数据库名
     'database'        => 'weixin',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => '1111111111111111',
+    'password'        => '2222222',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
