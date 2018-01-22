4083beda1de20a3ac0033982f91d31fccd4e0b38
zeng342541425 <342541425@qq.com>
2018-01-22 15:21:06 +0800

    修改数据库文件

--- application/database.php
+++ application/database.php
@@ -13,7 +13,7 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => '2222222222',
+    'hostname'        => '444',
     // 数据库名
     'database'        => 'weixin',
     // 用户名
