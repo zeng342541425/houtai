00cb95250d5605c73b5a2e0328ea82270ac143af
zeng342541425 <342541425@qq.com>
2018-01-22 15:00:53 +0800

    aa

--- application/database.php
+++ application/database.php
@@ -13,13 +13,13 @@ return [
     // 数据库类型
     'type'            => 'mysql',
     // 服务器地址
-    'hostname'        => 'xxxxxxxxx',
+    'hostname'        => '111.231.136.181',
     // 数据库名
     'database'        => 'weixin',
     // 用户名
     'username'        => 'root',
     // 密码
-    'password'        => 'xxxxxxxxx',
+    'password'        => 'rnSSrPo2WmZC7RH',
     // 端口
     'hostport'        => '3306',
     // 连接dsn
